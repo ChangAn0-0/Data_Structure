0c8d8283037a3d7b0f7de86d27975047bb71ab59
ChangAn <1533167307@qq.com>
2022-08-22 17:44:42 +0800

    0-0

--- learning/Chapter-one/Maximum_subcolumn_sum.c
+++ learning/Chapter-one/Maximum_subcolumn_sum.c
@@ -8,6 +8,11 @@
 void transfer(int (*ptr)(int num[]), int num[], int n);
 int Maxsubsum1(int num[]);
 int Maxsubsum2(int num[]);
+int Maxsubsum3(int num[]);
+int Maxsubsum4(int num[]);
+
+int divide(int num[], int left, int right);
+int Max3(int a, int b, int c);
 
 int main(void)
 {
@@ -21,9 +26,10 @@ int main(void)
 			num[i] = i;
 	}
 
-	transfer(Maxsubsum1, num, 1);
-	transfer(Maxsubsum2, num, 2);
-
+	//transfer(Maxsubsum1, num, 1);
+	//transfer(Maxsubsum2, num, 2);
+	transfer(Maxsubsum3, num, 3);
+	transfer(Maxsubsum4, num, 4);
 
 	return 0;
 }
@@ -31,15 +37,15 @@ int main(void)
 //调用算法函数并输出该算法用时
 void transfer(int (*ptr)(int num[]), int num[],int n)
 {
-	int max; double duration;
+	int max=0; double duration;
 	clock_t start, stop;
 	
 	start = clock();
 	max = ptr(num);
 	stop = clock();
-	duration = (double)stop - start;
+	duration = ((double)stop - start);
 	
-	printf("Maxsum%d:%d duration:%2.6e\n",n,max, duration);
+	printf("Maxsum%d:%d duration:%2.9e\n",n,max, duration);
 }
 
 //算法一：穷举法
@@ -84,5 +90,66 @@ int Maxsubsum2(int num[])
 //算法三：分治法，
 int Maxsubsum3(int num[])
 {
+	return divide(num, 0, MAXN - 1);
+}
 
+int divide(int num[], int left, int right)
+{
+	int Maxleftsum, Maxrightsum;
+	int Maxleftbordesum, Maxrightbordesum;
+
+	int Leftbordesum, Rightbordesum;
+	int center, i;
+
+	if (left = right){
+		if (num[left] > 0)
+			return num[left];
+		else
+			return 0;
+	}
+
+	center = (left + right) / 2;
+
+	Maxleftsum = divide(num, left, center);
+	Maxrightsum = divide(num, center + 1, right);
+
+	Maxleftbordesum = 0; Maxrightbordesum = 0;
+	Leftbordesum = 0; Rightbordesum = 0;
+	for (i = center; i >= left; i--)
+	{
+		Leftbordesum += num[i];
+		if (Leftbordesum > Maxleftbordesum)
+			Maxleftbordesum = Leftbordesum;
+	}
+
+	for (i = center; i <= right; i++)
+	{
+		Rightbordesum += num[i];
+		if (Rightbordesum > Maxrightbordesum)
+			Maxrightbordesum = Rightbordesum;
+	}
+
+	return Max3(Maxleftsum, Maxrightsum, Maxleftbordesum + Maxrightbordesum);
+}
+
+int Max3(int a, int b, int c)
+{
+	return a > b ? a > c ? a : c : b > c ? b : c;
+}
+
+//算法四：在线算法
+int Maxsubsum4(int num[])
+{
+	int i;
+	int ThisSum=0, Maxsum=0;
+
+	for (i = 0; i < MAXN; i++)
+	{
+		ThisSum += num[i];
+		if (ThisSum < 0)
+			ThisSum = 0;
+		if (ThisSum > Maxsum)
+			Maxsum = ThisSum;
+	}
+	return Maxsum;
 }
